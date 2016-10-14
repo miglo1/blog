ddfe2c57681040572d2aac39c7bba3ba355e100a
miglo1 <d13716722370@163.com>
2016-10-14 16:23:29 +0800

    module.exports

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 require('../css/reset.css') ;
 require('../css/common.css');
 require('../css/index.css');
+var dialog=require('../public/dialog1.js');
+// var $=require('jquery');
+dialog();
+// $(".wrapper").css("background","red");
---
+++ public/dialog1.js
@@ -0,0 +1,4 @@
+// Greeter.js
+module.exports = function() {
+  console.log(2222);
+};
